0f87f38ea319cf7b6922da22105c3d610f6844bd
BeamBeam986 <640108020010@dpu.ac.th>
2022-06-14 10:34:51 +0700

    ฺBeam

--- oop1/lightswitch_oop.py
+++ oop1/lightswitch_oop.py
@@ -1,7 +1,4 @@
 
-from re import T
-
-
 class LingtSwitch():
     def __init__(self) -> None:
         self.switch_status= False
@@ -9,19 +6,19 @@ class LingtSwitch():
     def turnon(self):
         self.switch_status= True
 
-    def turnon(self):
+    def turnoff(self):
         self.switch_status= False
 
     def show(self):
-        print (f'stutus = self.switch_status')
+        print (f'stutus = {self.switch_status}')
 
 #สร้างวัตถุ object จากแม่พิมพ์ class
-switch_obj = LightSwitch()
+switch_obj = LingtSwitch()
 #เรียกใช้เมธอดฟังก์ชั้น
 switch_obj.show() #F
 switch_obj.turnon()
 switch_obj.show() #T
-switch_obj.turoff()
+switch_obj.turnoff()
 switch_obj.show() #f
-switch_obj.turoff()
+switch_obj.turnoff()
 switch_obj.show()#f
---
+++ oop1/rectangle.py
@@ -0,0 +1,7 @@
+#รับ input
+width = float (input("Enter width: "))
+length = float (input("Enter width: "))
+#หาพื้นที่
+area = width * length
+#แสดงผล
+print(f"Rectangele area : {area}")
