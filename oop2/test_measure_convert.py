65a19706537e508b938b1ebbe3e400367ebecbad
BeamBeam986 <640108020010@dpu.ac.th>
2022-06-20 14:42:38 +0700

    mymath

---
+++ oop2/test_measure_convert.py
@@ -0,0 +1,11 @@
+"""import measure_convert as mc
+
+if __name__ =='__main__':
+    print(mc.inch_cm(12.5))#12.5นิ้ว =?
+    print(mc.cm_inch(100))#100 ซม =?"""
+
+from oop2.measure_convert import inch_cm,cm_inch
+
+if __name__ =='__main__':
+    print(inch_cm(12.5))
+    print(cm_inch(100))
