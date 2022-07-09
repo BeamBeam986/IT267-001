edb35b69a9013c2c9f08c11a68ca479b9db18029
BeamBeam986 <640108020010@dpu.ac.th>
2022-07-09 14:49:31 +0700

    [[

---
+++ oop2/abstraction/country/country/countrystract.py
@@ -0,0 +1,13 @@
+#countryabstract.py
+from abc import ABC,abstractmethod
+class Country(ABC):
+    def __init__(self,name,pop) -> None:
+        super().__init__()
+        self.name = name
+        self.population = pop
+    
+    @abstractmethod
+    def show_detail(self):
+        pass
+
+#หน้า92
