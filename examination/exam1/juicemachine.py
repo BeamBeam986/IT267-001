b165cf536cadcac553421c42c0b2f982369ee198
BeamBeam986 <640108020010@dpu.ac.th>
2022-07-10 12:00:28 +0700

    exam

--- examination/exam1/juicemachine.py
+++ examination/exam1/juicemachine.py
@@ -2,8 +2,8 @@ class JuiceOrder:
     menu_type = "Juiceorder"
     total = 0
     def __init__(self,munu:str,size:str,price:int) -> None:
-        self.munu = munu.upper()
-        self.size = size.upper()
+        self.munu = munu
+        self.size = size
         self.price = 0
 
 def check_menu(self):
@@ -25,23 +25,23 @@ def compule_price(self):
         else:
             self.price
         
-        JuiceOrder.total = self.price + self.size
+        JuiceOrder.total = (self.price + self.size)
 
 
 def display_order(self):
     self.check_munu()
     self.compute_price()
-    return f' {self.menu} ,{self.size} +{self.price}=> {JuiceOrder.total}'
+    return f' {self.menu} ,{self.size} +{self.price} => {JuiceOrder.total}'
 
 def __del__(self):
         print("Object was destroyed")
 
 
 if __name__ == "__main__":
-    order1 = JuiceOrder('WJ(L*35=> 35 baht')
-    order2 = JuiceOrder('OJ(L*25=> 25 baht')
-    order3 = JuiceOrder('PJ(L*35=> 35 baht')
+    order1 ='WJ(L*35=> 35 baht'
+    order2 ='OJ(L*25=> 25 baht'
+    order3 ='PJ(L*35=> 35 baht'
     
-    print(order1.display_order())
-    print(order2.display_order())
-    print(order3.display_order())
+    print(order1)
+    print(order2)
+    print(order3)
